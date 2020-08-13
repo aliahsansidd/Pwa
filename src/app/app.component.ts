3fc5f71e969c3891fe1178ab54816825f5a7a44e
Ahsan Ali Siddiqui <ahsan.siddiqui@systemsltd.com>
2020-08-13 05:01:40 -0700

    pwa check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { SwUpdate } from '@angular/service-worker';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'PWA';
+  employee: any = [];
+  constructor(private swupdate: SwUpdate,private httpCient:HttpClient) {
+    this.swupdate.available.subscribe((event: any) => {
+      // reload / refresh the browser
+      this.swupdate.activateUpdate().then(() => document.location.reload());
+    });
+    this.getData();
+  }
+
+  getData(){
+    this.httpCient.get("https://reqres.in/api/users").subscribe((res:any)=>{
+this.employee = res.data;
+    });
+  }
 }
